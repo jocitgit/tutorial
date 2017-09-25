38715827c4adba9c3fc1f1d8b0f140f4eb519073
Joella Flood <joella.flood@mycit.ie>
2017-09-25 11:20:26 +0100

    made speculative changes

--- src/power.py
+++ src/power.py
@@ -2,12 +2,15 @@
     the power of another.
 """
 def to_the_power(x, y = 2):
-    result = x
-    for i in range(0, y):
-        result = result * x
-    return result
+	"""trying this out
+	"""
+	return x**y
+ #   result = x
+ #   for i in range(0, y):
+ #       result = result * x
+ #   return result
 
-x = 9
+x = 5 #
 y = 8 #
 
 print("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y)))
